d129bb696db19b4709a44783bcb0d57768690af3
Alexander Morales <amoralesmm@yahoo.com>
2021-09-12 16:42:19 -0500

    Added colliders
    Player, water and stands colliders were added.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
     //public Rigidbody2D playerRigidBody2d; Public can be access by other classes and show on the inspector.
     [SerializeField] Rigidbody2D playerRigidBody; //SerializedField shows on the inspector but can't be access by other classes
-    [SerializeField] float moveSpeed = 0;
+    [SerializeField] float moveSpeed = 1;
     [SerializeField] Animator playerAnimator;
 
 
@@ -23,7 +23,7 @@ public class Player : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        playerRigidBody.velocity = new Vector2(horizontalInput, verticalInput);
+        playerRigidBody.velocity = new Vector2(horizontalInput, verticalInput) * moveSpeed;
         playerAnimator.SetFloat("movementX", playerRigidBody.velocity.x);
         playerAnimator.SetFloat("movementY", playerRigidBody.velocity.y);
 
