c3fc34d7476192ee7db3b5551b63506a7b586105
Alexander Morales <amoralesmm@yahoo.com>
2021-09-18 03:26:58 -0500

    Added Tilemap Road

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using Cinemachine;
 public class CameraController : MonoBehaviour
 {
+    private Player playerTarget;
+    CinemachineVirtualCamera virtualCamera;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTarget = FindObjectOfType<Player>();
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();   
+
+        virtualCamera.Follow = playerTarget.transform;
     }
 
     // Update is called once per frame
