be6b6d3216022739a1e48af133c0f2a448d6416e
Alexander Morales <amoralesmm@yahoo.com>
2021-09-20 01:30:55 -0500

    Created a LevelManager, and Worked on Tilemap layers.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class LevelManager : MonoBehaviour 
+{
+    [SerializeField] Tilemap tilemap;
+    private Vector3 bottomLeftEdge;
+    private Vector3 topRightEdge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        bottomLeftEdge = tilemap.localBounds.min + new Vector3(0.5f, 0.5f, 0f);
+        topRightEdge = tilemap.localBounds.max + new Vector3(-0.5f, -0.5f, 0f);
+        Player.instance.SetLimit(bottomLeftEdge, topRightEdge);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Player : MonoBehaviour
 {
@@ -9,8 +10,11 @@ public class Player : MonoBehaviour
     [SerializeField] Rigidbody2D playerRigidBody; //SerializedField shows on the inspector but can't be access by other classes
     [SerializeField] float moveSpeed = 1;
     [SerializeField] Animator playerAnimator;
+
     public string transitionName;
 
+    private Vector3 bottomLeftEdge;
+    private Vector3 topRightEdge;
 
     // Start is called before the first frame update
     void Start()
@@ -24,13 +28,14 @@ public class Player : MonoBehaviour
             instance = this;
         }
         
-        DontDestroyOnLoad(gameObject);   
+        DontDestroyOnLoad(gameObject);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
@@ -39,5 +44,16 @@ public class Player : MonoBehaviour
         playerAnimator.SetFloat("movementY", playerRigidBody.velocity.y);
 
 
+        transform.position = new Vector3
+            ( 
+            Mathf.Clamp(transform.position.x, bottomLeftEdge.x, topRightEdge.x), 
+            Mathf.Clamp(transform.position.y, bottomLeftEdge.y, topRightEdge.y),
+            Mathf.Clamp(transform.position.z, bottomLeftEdge.z, topRightEdge.z)
+            );
+    }
+    public void SetLimit(Vector3 bottomEdgeToSet, Vector3 topEdgeToSet)
+    {
+        bottomLeftEdge = bottomEdgeToSet;
+        topRightEdge = topEdgeToSet;
     }
 }
